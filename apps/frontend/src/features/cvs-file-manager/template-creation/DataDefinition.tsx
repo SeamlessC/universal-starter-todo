9e47cbeafac631a6d4738104b862d85151ecea07
maneesha_lakshani <maneeshalakshani9@gmail.com>
2023-02-23 13:02:27 +0530

    feat: Data Definition form

--- apps/frontend/src/features/cvs-file-manager/template-creation/DataDefinition.tsx
+++ apps/frontend/src/features/cvs-file-manager/template-creation/DataDefinition.tsx
@@ -1,4 +1,4 @@
-import { Accordion } from '@mantine/core'
+import { Accordion, ScrollArea } from '@mantine/core'
 
 import AddDimensionForm from './forms/AddDimensionForm'
 import AddMetricForm from './forms/AddMetricForm'
@@ -8,35 +8,37 @@ import FindCustomerIDForm from './forms/FindCustomerIDForm'
 function DataDefinition() {
   return (
     <div style={{width: "100%"}}>
-      <Accordion>
-        <Accordion.Item value="Define Date">
-          <Accordion.Control>Define Date</Accordion.Control>
-          <Accordion.Panel>
-            <DefineDateForm />
-          </Accordion.Panel>
-        </Accordion.Item>
+      <ScrollArea>
+        <Accordion>
+          <Accordion.Item value="Define Date">
+            <Accordion.Control>Define Date</Accordion.Control>
+            <Accordion.Panel>
+              <DefineDateForm />
+            </Accordion.Panel>
+          </Accordion.Item>
+    
+          <Accordion.Item value="Find Customer ID">
+            <Accordion.Control>Find Customer ID</Accordion.Control>
+            <Accordion.Panel>
+              <FindCustomerIDForm />
+            </Accordion.Panel>
+          </Accordion.Item>
+    
+          <Accordion.Item value="Add Dimensions">
+            <Accordion.Control>Add Dimensions</Accordion.Control>
+            <Accordion.Panel>
+              <AddDimensionForm />
+            </Accordion.Panel>
+          </Accordion.Item>
   
-        <Accordion.Item value="Find Customer ID">
-          <Accordion.Control>Find Customer ID</Accordion.Control>
-          <Accordion.Panel>
-            <FindCustomerIDForm />
-          </Accordion.Panel>
-        </Accordion.Item>
-  
-        <Accordion.Item value="Add Dimensions">
-          <Accordion.Control>Add Dimensions</Accordion.Control>
-          <Accordion.Panel>
-            <AddDimensionForm />
-          </Accordion.Panel>
-        </Accordion.Item>
-
-        <Accordion.Item value="Add Metric">
-          <Accordion.Control>Add Metric</Accordion.Control>
-          <Accordion.Panel>
-            <AddMetricForm />
-          </Accordion.Panel>
-        </Accordion.Item>
-      </Accordion>
+          <Accordion.Item value="Add Metric">
+            <Accordion.Control>Add Metric</Accordion.Control>
+            <Accordion.Panel>
+              <AddMetricForm />
+            </Accordion.Panel>
+          </Accordion.Item>
+        </Accordion>
+      </ScrollArea>
     </div>
   )
 }
--- apps/frontend/src/features/cvs-file-manager/template-creation/forms/DefineDateForm.tsx
+++ apps/frontend/src/features/cvs-file-manager/template-creation/forms/DefineDateForm.tsx
@@ -1,8 +1,65 @@
+import { Group, NumberInput, Select } from '@mantine/core'
 import React from 'react'
+import Btn from '../../../profile-ui/Btn'
+import CustomeLink from '../../../profile-ui/Links'
 
 function DefineDateForm() {
   return (
-    <div>DefineDateForm</div>
+    <div>
+      <Select
+        label="Type"
+        defaultValue="react"
+        data={[
+          { value: 'react', label: 'React' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+        ]}
+      />
+      <div style={{display: "flex", flexDirection: "row"}}>
+        <NumberInput
+          mt={5}
+          defaultValue={0}
+          label="Column"
+          min={0}
+          width="50%"
+        />
+        <NumberInput
+          mt={5}
+          ml={5}
+          defaultValue={0}
+          label="Line"
+          min={0}
+          width="50%"
+        />
+      </div>
+      <Select
+        mt={5}
+        label="Format"
+        defaultValue="MM DD YYYY"
+        data={[
+          { value: 'MM DD YYYY', label: 'MM DD YYYY' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+        ]}
+      />
+      <Select
+        mt={5}
+        label="Range seperator"
+        defaultValue="none"
+        data={[
+          { value: 'none', label: 'None' },
+          { value: 'ng', label: 'Angular' },
+          { value: 'svelte', label: 'Svelte' },
+          { value: 'vue', label: 'Vue' },
+        ]}
+      />
+      <Group position='apart' mt={10}>
+        <CustomeLink label='Cancel' />
+        <Btn label="Apply" />
+      </Group>
+    </div>
   )
 }
 
