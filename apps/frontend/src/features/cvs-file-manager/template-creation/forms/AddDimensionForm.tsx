17a9c1da9a58f8483e03e75ffdcca0008c00fb5a
maneesha_lakshani <maneeshalakshani9@gmail.com>
2023-02-24 11:00:02 +0530

    feat: add dimensions form

--- apps/frontend/src/features/cvs-file-manager/template-creation/forms/AddDimensionForm.tsx
+++ apps/frontend/src/features/cvs-file-manager/template-creation/forms/AddDimensionForm.tsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import { Group, NumberInput, TextInput } from '@mantine/core'
+
+import Btn from '../../../profile-ui/Btn'
+import CustomeLink from '../../../profile-ui/Links'
 
 function AddDimensionForm() {
   return (
-    <div>AddDimensionForm</div>
+    <>
+      <TextInput
+        label="Name"
+      />
+      <NumberInput
+        mt={5}
+        defaultValue={0}
+        label="Column"
+        min={0}
+      />
+      <Group position='apart' mt={10} mb={20}>
+        <CustomeLink label='Cancel' />
+        <Btn label="Apply" varient="outline" />
+      </Group>
+    </>
   )
 }
 
